405b34947df27d5dc62a218f4b877eb3130bafb3
Eliass-g <eliass.ghauss@gmail.com>
2023-10-02 23:06:36 -0400

    added url short part 2

--- express_server.js
+++ express_server.js
@@ -49,6 +49,13 @@ app.listen(PORT, () => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  const id = generateRandomString();
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
